366a01575f6b6cce252a21f65037f2d499a54641
rolwinpaul <rolwinpaul@gmail.com>
2024-03-24 20:53:14 +0300

    fix: api and schema migration

--- app/(pages)/book/page.tsx
+++ app/(pages)/book/page.tsx
@@ -7,7 +7,7 @@ const book = () => {
       <form className="bg-blue-50 p-6 rounded-lg mt-10 flex flex-col gap-4 mx-8" action="">
         <fieldset className="flex bg-blue-100 w-full h-full  p-3 rounded-xl items-center justify-between">
           <label htmlFor="timing" className="font-bold ">Select Timing</label>
-          <select  className="p-2 rounded-xl" name="timing" id="timing">
+          <select  className="p-2 rounded-xl bg-white" name="timing" id="timing">
             <option className="p-2" value="1">9:30-1:30</option>
             <option className="p-2" value="2">1:30-5:30</option>
           </select>
